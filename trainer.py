a2a9c32025e43150c1a7871b4f83e08b8fb64606
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-04 13:33:34 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -32,14 +32,17 @@ class Trainer(object):
         os.makedirs(self.images_dir, exist_ok=True)
 
         ### Make Models ###
-        if self.p.biggan:
+        if  self.p.biggan:
+            print('Using BigGAN')
             self.netD = BigD(self.p).to(self.device)
             self.netG = BigG(self.p).to(self.device)
         elif self.p.hybrid:
+            print('Using Hybrid Model')
             self.netG = BigG(self.p).to(self.device)
             self.netD = Discriminator(self.p).to(self.device)
             self.netD.apply(self.weights_init)
         else:
+            print('Using DCGAN')
             self.netG = Generator(self.p).to(self.device)
             self.netG.apply(self.weights_init)
             self.netD = Discriminator(self.p).to(self.device)
