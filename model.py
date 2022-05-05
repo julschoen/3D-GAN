d2f6d724179f2c0fd30dc0dfa9f19dfda5095b8f
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-05 13:29:36 +0200

    Update model.py

--- model.py
+++ model.py
@@ -169,9 +169,5 @@ class Discriminator(nn.Module):
             )
 
     def forward(self, input):
-        output = self.main(input)
-        if not self.sagan:
-            output = output.mean(0)
-            output = output.view(1)
-        
+        output = self.main(input)        
         return output
