6910ffe46bf9fd26313ac3a15e889cc83af60b7e
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-06 13:46:02 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -75,20 +75,12 @@ class Trainer(object):
 
 
     def weights_init(self, m):
-        if self.p.sagan:
-            classname = m.__class__.__name__
-            if classname.find('Conv') != -1:
-                nn.init.orthogonal_(m.weight)
-            elif classname.find('BatchNorm') != -1:
-                nn.init.normal_(m.weight.data, 1.0, 0.02)
-                nn.init.constant_(m.bias.data, 0)
-        else:
-            classname = m.__class__.__name__
-            if classname.find('Conv') != -1:
-                nn.init.normal_(m.weight.data, 0.0, 0.02)
-            elif classname.find('BatchNorm') != -1:
-                nn.init.normal_(m.weight.data, 1.0, 0.02)
-                nn.init.constant_(m.bias.data, 0)
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
         
     def log_train(self, step, fake, real, D_x, D_G_z1, D_G_z2):
         with torch.no_grad():
