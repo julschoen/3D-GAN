dde9fe20104adbd60dc5ec72b609ba57456ba267
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-12 13:18:16 +0200

    Update eval_utils.py

--- eval_utils.py
+++ eval_utils.py
@@ -16,6 +16,8 @@ def psnr(real, fake):
 def ssim(real, fake):
     real = (real+1)/2
     fake = (fake+1)/2
+    ms_ssim_module = MS_SSIM(data_range=1, size_average=True, channel=1)
+    ms_ssim_module(real, fake)
     #print(real)
     #print(fake)
     return 1#ms_ssim(real.to(torch.float32), fake.to(torch.float32), data_range=1, size_average=True)
