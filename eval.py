19fc2f92f31139bbd1ef19cba2d5691ceeba58af
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-12 10:08:19 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -36,9 +36,9 @@ def eval(params):
 		ssims = []
 		psnrs = []
 		fids = []
-    	for i, data in enumerate(generator):
-    		x1 = data.unsqueeze(dim=1)
-    		noise = torch.randn(4, netG.dim_z, 1, 1,1,dtype=torch.float, device=params.device)
+		for i, data in enumerate(generator):
+			x1 = data.unsqueeze(dim=1)
+			noise = torch.randn(4, netG.dim_z, 1, 1,1,dtype=torch.float, device=params.device)
     		x2 = netG(noise)
     		s,p,f = ssim(x1,x2), psnr(x1.cpu(),x2.cpu()),fid_3d(fid_model, x1, x2)
     		ssims.append(s)
