f182b4156c9765ed64c5b3a6d1ec1859882ff61b
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-18 15:13:19 +0200

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -5,7 +5,7 @@ from torch.utils.data.dataset import Dataset
 
 class DATA(Dataset):
   def __init__(self, path): 
-    self.data = np.load(path)['x']
+    self.data = np.load(path)['X']
     self.len = self.data.shape[0]
 
   def __getitem__(self, index):
