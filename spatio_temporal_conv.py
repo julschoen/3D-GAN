25a45d7f9c74fdc1e99feda896aae660c93cae67
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-30 14:36:19 +0200

    Update spatio_temporal_conv.py

--- spatio_temporal_conv.py
+++ spatio_temporal_conv.py
@@ -6,47 +6,36 @@ import torch.nn.utils.spectral_norm as SpectralNorm
 
 
 class SpatioTemporalConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=True):
-        super(SpatioTemporalConv, self).__init__()
+    def __init__(self, in_channels, out_channels, kernel_size, stride=(1, 1, 1),
+                        padding=(0, 0, 0), dilation=(1, 1, 1), bias=True):
+        super().__init__()
+        t = kernel_size[0]
+        d = (kernel_size[1] + kernel_size[2])//2
+        self.in_channels = in_channels
+        self.out_channels = out_channels
 
-        # if ints are entered, convert them to iterables, 1 -> [1, 1, 1]
-        kernel_size = _triple(kernel_size)
-        stride = _triple(stride)
-        padding = _triple(padding)
+        #Hidden size estimation to get a number of parameter similar to the 3d case
+        self.hidden_size = int((t*d**2*in_channels*out_channels)/(d**2*in_channels+t*out_channels))
 
-        # decomposing the parameters into spatial and temporal components by
-        # masking out the values with the defaults on the axis that
-        # won't be convolved over. This is necessary to avoid unintentional
-        # behavior such as padding being added twice
-        spatial_kernel_size =  [1, kernel_size[1], kernel_size[2]]
-        spatial_stride =  [1, stride[1], stride[2]]
-        spatial_padding =  [0, padding[1], padding[2]]
-
-        temporal_kernel_size = [kernel_size[0], 1, 1]
-        temporal_stride =  [stride[0], 1, 1]
-        temporal_padding =  [padding[0], 0, 0]
-
-        # compute the number of intermediary channels (M) using formula 
-        # from the paper section 3.5
-        intermed_channels = int(math.floor((kernel_size[0] * kernel_size[1] * kernel_size[2] * in_channels * out_channels)/ \
-                            (kernel_size[1]* kernel_size[2] * in_channels + kernel_size[0] * out_channels)))
-
-        # the spatial conv is effectively a 2D conv due to the 
-        # spatial_kernel_size, followed by batch_norm and ReLU
-        self.spatial_conv = nn.Conv3d(in_channels, intermed_channels, spatial_kernel_size,
-                                    stride=spatial_stride, padding=spatial_padding, bias=bias)
-        self.bn = nn.BatchNorm3d(intermed_channels)
-        self.relu = nn.ReLU()
-
-        # the temporal conv is effectively a 1D conv, but has batch norm 
-        # and ReLU added inside the model constructor, not here. This is an 
-        # intentional design choice, to allow this module to externally act 
-        # identical to a standard Conv3D, so it can be reused easily in any 
-        # other codebase
-        self.temporal_conv = nn.Conv3d(intermed_channels, out_channels, temporal_kernel_size, 
-                                    stride=temporal_stride, padding=temporal_padding, bias=bias)
+        self.conv2d = SpectralNorm(nn.Conv2d(in_channels, self.hidden_size, kernel_size[1:], stride[1:], padding[1:], bias=bias))
+        self.conv1d = SpectralNorm(nn.Conv1d(self.hidden_size, out_channels, kernel_size[0], stride[0], padding[0], bias=bias))
 
     def forward(self, x):
-        x = self.relu(self.bn(self.spatial_conv(x)))
-        x = self.temporal_conv(x)
+        #2D convolution
+        b, c, t, d1, d2 = x.size()
+        x = x.permute(0,2,1,3,4).contiguous()
+        x = x.view(b*t, c, d1, d2)
+        x = F.relu(self.conv2d(x))
+        
+        #1D convolution
+        c, dr1, dr2 = x.size(1), x.size(2), x.size(3)
+        x = x.view(b, t, c, dr1, dr2)
+        x = x.permute(0, 3, 4, 2, 1).contiguous()
+        x = x.view(b*dr1*dr2, c, t)
+        x = self.conv1d(x)
+
+        #Final output
+        out_c, out_t = x.size(1), x.size(2)
+        x = x.view(b, dr1, dr2, out_c, out_t)
+        x = x.permute(0, 3, 4, 1, 2).contiguous()
         return x
