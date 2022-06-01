65ec47969ca41be7abb5276e3d44765b1a0577b0
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-01 14:36:39 +0200

    Update acs_conv.py

--- acs_conv.py
+++ acs_conv.py
@@ -6,33 +6,6 @@ import collections.abc
 from itertools import repeat
 import math
 
-class ACSConv(_ACSConv):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, acs_kernel_split=None, 
-                 bias=True, padding_mode='zeros'):
-        super().__init__(
-            in_channels, out_channels, kernel_size, stride, padding, dilation,
-            False, 0, groups, bias, padding_mode)
-        if acs_kernel_split is None:
-            if self.out_channels%3==0:
-                self.acs_kernel_split = (self.out_channels//3,self.out_channels//3,self.out_channels//3)
-            if self.out_channels%3==1:
-                self.acs_kernel_split = (self.out_channels//3+1,self.out_channels//3,self.out_channels//3)
-            if self.out_channels%3==2:
-                self.acs_kernel_split = (self.out_channels//3+1,self.out_channels//3+1,self.out_channels//3)
-        else:
-            self.acs_kernel_split = acs_kernel_split
-
-
-    def forward(self, x):
-        return acs_conv_f(x, self.weight, self.bias, self.kernel_size, self.dilation, self.padding, self.stride, 
-                            self.groups, self.out_channels, self.acs_kernel_split)
-
-
-    def extra_repr(self):
-        s = super().extra_repr() + ', acs_kernel_split={acs_kernel_split}'
-        return s.format(**self.__dict__)
-
 def conv3D_output_shape_f(i, input_shape, kernel_size, dilation, padding, stride):
     """
     Calculate the original output size assuming the convolution is nn.Conv3d based on 
@@ -127,6 +100,7 @@ def _to_ntuple(n):
                 return x
     return parse
 
+
 class _ACSConv(nn.Module):
     """
     Base class for ACS Convolution
@@ -199,3 +173,30 @@ class _ACSConv(nn.Module):
         super().__setstate__(state)
         if not hasattr(self, 'padding_mode'):
             self.padding_mode = 'zeros'
+
+class ACSConv(_ACSConv):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, acs_kernel_split=None, 
+                 bias=True, padding_mode='zeros'):
+        super().__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation,
+            False, 0, groups, bias, padding_mode)
+        if acs_kernel_split is None:
+            if self.out_channels%3==0:
+                self.acs_kernel_split = (self.out_channels//3,self.out_channels//3,self.out_channels//3)
+            if self.out_channels%3==1:
+                self.acs_kernel_split = (self.out_channels//3+1,self.out_channels//3,self.out_channels//3)
+            if self.out_channels%3==2:
+                self.acs_kernel_split = (self.out_channels//3+1,self.out_channels//3+1,self.out_channels//3)
+        else:
+            self.acs_kernel_split = acs_kernel_split
+
+
+    def forward(self, x):
+        return acs_conv_f(x, self.weight, self.bias, self.kernel_size, self.dilation, self.padding, self.stride, 
+                            self.groups, self.out_channels, self.acs_kernel_split)
+
+
+    def extra_repr(self):
+        s = super().extra_repr() + ', acs_kernel_split={acs_kernel_split}'
+        return s.format(**self.__dict__)
