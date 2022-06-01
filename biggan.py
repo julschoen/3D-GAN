b4ba00413902e7e83ae846f197b2025e7e0b7906
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-01 14:42:06 +0200

    Update biggan.py

--- biggan.py
+++ biggan.py
@@ -74,25 +74,15 @@ class Discriminator(nn.Module):
     self.p = params
     self.dim_z = self.p.z_size
     # Architecture
-    #self.arch = {'in_channels' :  [item * self.p.filterD for item in [1, 2, 4,  8, 16]],
-    #           'out_channels' : [item * self.p.filterD for item in [2, 4, 8, 16, 16]],
-    #           'downsample' : [True] * 5 + [False],
-    #           'resolution' : [64, 32, 16, 8, 4, 4],
-    #           'attention' : {2**i: 2**i in [int(item) for item in '16'.split('_')]
-    #                          for i in range(2,8)}}
-
-    
-    self.arch  = {'in_channels' :  [self.p.filterD*item for item in [1, 2, 4, 8]],
-               'out_channels' : [item * self.p.filterD for item in [2, 4, 8, 16]],
-               'downsample' : [True] * 4 + [False],
-               'resolution' : [32, 16, 8, 4, 4],
+    self.arch = {'in_channels' : [1]+[item * self.p.filterD for item in [1, 2, 4,  8, 16]],
+               'out_channels' : [item * self.p.filterD for item in [1,2, 4, 8, 16, 16]],
+               'downsample' : [True] * 5 + [False],
+               'resolution' : [64, 32, 16, 8, 4, 4],
                'attention' : {2**i: 2**i in [int(item) for item in '16'.split('_')]
-                              for i in range(2,7)}}
+                              for i in range(2,8)}}
+
     
     # Prepare model
-    self.msl = RandomCrop3D(device=params.device)
-    self.input_conv = snconv3d(1, self.arch['in_channels'][0])
-
     self.blocks = []
     for index in range(len(self.arch['out_channels'])):
       self.blocks += [[DBlock(in_channels=self.arch['in_channels'][index] if d_index==0 else self.arch['out_channels'][index],
@@ -120,8 +110,7 @@ class Discriminator(nn.Module):
 
   def forward(self, x):
     # Run input conv
-    x = self.msl(x)
-    h = self.input_conv(x)
+    h = x
     # Loop over blocks
     for index, blocklist in enumerate(self.blocks):
       for block in blocklist:
