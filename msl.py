bef097c4fa96ebf591399043e03f5774092f6bd6
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-13 15:07:58 +0200

    Update msl.py

--- msl.py
+++ msl.py
@@ -119,9 +119,9 @@ class RandomCrop3D(torch.nn.Module):
         self.n_crops = n_crops
         
     def __call__(self, x):
-        x_ = self.crop(x[0]).unsqueeze(1)
+        x_ = self.crop(x[0].clone()).unsqueeze(1)
         for xi in x[1:]:
-            xi_ = self.crop(xi).unsqueeze(1)
+            xi_ = self.crop(xi.clone()).unsqueeze(1)
             x_ = torch.concat((x_, xi_))
         return x_
 
@@ -130,16 +130,16 @@ class RandomCrop3D(torch.nn.Module):
         slice_hwd = [self._get_slice(i, k) for i, k in zip(self.img_sz, (crop_size, crop_size, crop_size))]
         x_ = self._crop(x.squeeze(), *slice_hwd)
         d = torch.linspace(-1,1,64)
-        meshz, meshy, meshx = torch.meshgrid((d, d, d))
-        grid = torch.stack((meshx, meshy, meshz), 3).unsqueeze(0).to(self.device)
+        meshz, meshy, meshx = torch.meshgrid((d, d, d), indexing='xy')
+        grid = torch.stack((meshx, meshy, meshz), 3).unsqueeze(0).unsqueeze(0).to(self.device)
         x_ = grid_sample(x_.unsqueeze(0).unsqueeze(0), grid).squeeze(0)
         for _ in range(self.n_crops-1):
             crop_size = int(torch.rand(1) * self.img_sz[0]) 
             slice_hwd = [self._get_slice(i, k) for i, k in zip(self.img_sz, (crop_size, crop_size, crop_size))]
             xi = self._crop(x.squeeze(), *slice_hwd)
             d = torch.linspace(-1,1,64)
-            meshz, meshy, meshx = torch.meshgrid((d, d, d))
-            grid = torch.stack((meshx, meshy, meshz), 3).unsqueeze(0).to(self.device)
+            meshz, meshy, meshx = torch.meshgrid((d, d, d), indexing='xy')
+            grid = torch.stack((meshx, meshy, meshz), 3).unsqueeze(0).unsqueeze(0).to(self.device)
             xi = grid_sample(xi.unsqueeze(0).unsqueeze(0), grid).squeeze(0)
             x_ = torch.cat((x_, xi))
         return x_
