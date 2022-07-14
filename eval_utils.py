0cb12fc9032fa2354d252fde871865200ae96dd5
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-14 12:29:53 +0200

    Update eval_utils.py

--- eval_utils.py
+++ eval_utils.py
@@ -7,10 +7,41 @@ import pytorch_fid_wrapper as FID
 from pytorch_msssim import MS_SSIM
 from torch.cuda.amp import autocast
 
-def mmd(real, fake):
+
+
+def guassian_kernel(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    n_samples = int(source.size()[0]) + int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+    total0 = total.unsqueeze(0).expand(
+        int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    total1 = total.unsqueeze(1).expand(
+        int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    L2_distance = ((total0-total1)**2).sum(2)
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul**i)
+                      for i in range(kernel_num)]
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp)
+                  for bandwidth_temp in bandwidth_list]
+    return sum(kernel_val)
+
+def mmd(self, source, target):
+    batch_size = int(source.size()[0])
+    kernels = self.guassian_kernel(
+        source, target, kernel_mul=self.kernel_mul, kernel_num=self.kernel_num, fix_sigma=self.fix_sigma)
+    XX = torch.mean(kernels[:batch_size, :batch_size])
+    YY = torch.mean(kernels[batch_size:, batch_size:])
+    XY = torch.mean(kernels[:batch_size, batch_size:])
+    YX = torch.mean(kernels[batch_size:, :batch_size])
+    loss = torch.mean(XX + YY - XY - YX)
+    return loss
+
+def mmd_(real, fake):
     x,y = real.squeeze(), fake.squeeze()
-    xt = torch.permute(x,(0, 3, 2, 1))
-    xx = torch.matmul(x, xt)
+    xx = torch.matmul(x, torch.permute(x,(0, 3, 2, 1)))
     yy = torch.matmul(y, torch.permute(y,(0, 3, 2, 1)))
     zz = torch.matmul(x, torch.permute(y,(0, 3, 2, 1)))
     rx = (xx.diag().unsqueeze(0).expand_as(xx))
