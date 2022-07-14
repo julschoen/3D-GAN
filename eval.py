11f01faf4a51f4bd09a93c0420699a2bd10dfa3a
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-14 12:08:21 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -57,27 +57,18 @@ def eval(params):
 					noise = torch.randn(data.shape[0], netG.dim_z,
 							1, 1, 1, dtype=torch.float, device=params.device)
 				x2 = netG(noise)
-				if i % 16 == 0 and i>0:
-					s,p,f = ssim(large_data,large_fake), psnr(large_data,large_fake),fid_3d(fid_model, large_data, large_fake)
-					m = mmd(large_data, large_fake)
-					ssims.append(s)
-					psnrs.append(p)
-					fids.append(f)
-					mmds.append(m)
-					large_data = None
-					large_fake = None
-				else:
-					if large_data is not None and large_fake is not None:
-						large_data = torch.concat((large_data, x1.cpu()))
-						large_fake = torch.concat((large_fake, x2.cpu()))
-					else:
-						large_data = x1.cpu()
-						large_fake = x2.cpu()
-				if i%5 == 0:
-					fa, fc, fs = fid(x1, x2, params.device)
-					fids_ax.append(fa)
-					fids_cor.append(fc)
-					fids_sag.append(fs)
+				
+				s,p,f = ssim(x1,x2), psnr(x1,x2),fid_3d(fid_model, x1, x2)
+				m = mmd(large_data, large_fake)
+				ssims.append(s)
+				psnrs.append(p)
+				fids.append(f)
+				mmds.append(m)
+
+				fa, fc, fs = fid(x1, x2, params.device)
+				fids_ax.append(fa)
+				fids_cor.append(fc)
+				fids_sag.append(fs)
 			
 
 		ssims = np.array(ssims)
