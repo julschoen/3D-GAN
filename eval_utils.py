0773cdf2a4a210f39c98700fac841f7445c47b55
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-14 12:22:12 +0200

    Update eval_utils.py

--- eval_utils.py
+++ eval_utils.py
@@ -9,8 +9,9 @@ from torch.cuda.amp import autocast
 
 def mmd(real, fake):
     x,y = real.squeeze(), fake.squeeze()
-    print(x,y)
-    xx, yy, zz = torch.mm(x, x.permute(0, 3, 1, 2)), torch.mm(y, y.permute(0, 3, 1, 2)), torch.mm(x, y.permute(0, 3, 1, 2))
+    xx = torch.mm(x, x.permute(0, 3, 1, 2))
+    yy = torch.mm(y, y.permute(0, 3, 1, 2))
+    zz = torch.mm(x, y.permute(0, 3, 1, 2))
     rx = (xx.diag().unsqueeze(0).expand_as(xx))
     ry = (yy.diag().unsqueeze(0).expand_as(yy))
 
