eb47b6d3b5c0689f2c2d0a8a0560aa82e1f729ba
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-15 12:10:44 +0200

    Update eval_metrics.py

--- eval_metrics.py
+++ eval_metrics.py
@@ -55,6 +55,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--batch_size', type=int, default=16, help='Batch size')
 	parser.add_argument('--data_path', type=str, default='test_lidc_128.npz',help='Path to data.')
+	parser.add_argument('--ngpu', type=int, default=2, help='Number of GPUs')
 	parser.add_argument('--device', type=str, default='cuda', help='Torch Device Choice')
 	parser.add_argument('--fid_checkpoint', type=str, default='resnet_50.pth', help='Path to pretrained MedNet')
 	params = parser.parse_args()
