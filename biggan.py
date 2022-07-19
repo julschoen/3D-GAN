cb845c6ac87dff6f6232a7ec28f6846a646f19cd
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-19 10:47:01 +0200

    Update biggan.py

--- biggan.py
+++ biggan.py
@@ -16,7 +16,7 @@ class Generator(nn.Module):
              'out_channels' : [item * self.p.filterG for item in [16, 8, 4,  2, 1]],
              'resolution' : [8, 16, 32, 64, 128],
              'attention' : {2**i: (2**i in [int(item) for item in '32'.split('_')]) for i in range(3,8)}}
-    print(self.arch)
+
     self.linear = snlinear(self.p.z_size, self.arch['in_channels'][0] * (4**3), sngan=self.p.sngan)
       
     self.blocks = []
