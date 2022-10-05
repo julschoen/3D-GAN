f623a4acb5ce6f22ee18b7bbd6087c88a2553f9f
julschoen <31760205+julschoen@users.noreply.github.com>
2022-10-05 14:01:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,16 @@ import numpy as np
 path =  'brats_128.npz'
 data = np.load(path)['X']
 
-print(data.shape)
+test_ind = np.random.choice(data, size=int(len(dirs)*0.1))
+train = []
+test = []
+for i, d in enumerate(data):
+	if i in test_ind:
+		test.append(d)
+	else:
+		train.append(d)
+
+train, test = np.array(train), np.array(test)
+
+np.savez_compressed('train_brats.npz', X=train)
+np.savez_compressed('test_brats.npz', X=test)
