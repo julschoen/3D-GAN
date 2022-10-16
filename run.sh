3a8f86aadba53917f62a616062a2cea8d5d8635c
julschoen <31760205+julschoen@users.noreply.github.com>
2022-10-16 19:56:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,3 @@
-../Temp-GAN/gan/bin/python main.py --log_dir=BigGAN3 --filterG=64 --filterD=64 --hinge=True --biggan=True
-
-../Temp-GAN/gan/bin/python main.py --log_dir=SAGAN2 --filterG=64 --filterD=64 --hinge=True --iterD=5 --lrG=1e-4 --dcgan=True --sagan=True
-../Temp-GAN/gan/bin/python main.py --log_dir=SAGAN3 --filterG=64 --filterD=64 --hinge=True --iterD=5 --lrG=1e-4 --dcgan=True --sagan=True
-
-../Temp-GAN/gan/bin/python main.py --log_dir=SNGAN2 --iterD=5 --lrG=1e-4 --dcgan=True --sngan=True
-../Temp-GAN/gan/bin/python main.py --log_dir=SNGAN3 --iterD=5 --lrG=1e-4 --dcgan=True --sngan=True
-
 ../Temp-GAN/gan/bin/python main.py --log_dir=WGAN2 --iterD=5 --lrG=1e-4 --dcgan=True
 ../Temp-GAN/gan/bin/python main.py --log_dir=WGAN3 --iterD=5 --lrG=1e-4 --dcgan=True
 
