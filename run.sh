1497684bdff6d1c9020825d5744aba2e3f38f476
julschoen <31760205+julschoen@users.noreply.github.com>
2022-10-27 17:40:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-../Temp-GAN/gan/bin/python main.py --log_dir=WGAN2 --iterD=5 --lrG=1e-4 --dcgan=True
-../Temp-GAN/gan/bin/python main.py --log_dir=WGAN3 --iterD=5 --lrG=1e-4 --dcgan=True
+../Temp-GAN/gan/bin/python main.py --log_dir=Style --iterD=2 --lrG=1e-4 --stylegan=True --hinge=True
 
-../Temp-GAN/gan/bin/python main.py --log_dir=MSL2 --iterD=5 --lrG=1e-4 --dcgan=True --msl=True
-../Temp-GAN/gan/bin/python main.py --log_dir=MSL3 --iterD=5 --lrG=1e-4 --dcgan=True --msl=True
