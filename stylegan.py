c7d34c1918a604218dea1cd4995a63b6beaee489
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-09 14:11:16 +0100

    Update stylegan.py

--- stylegan.py
+++ stylegan.py
@@ -93,6 +93,7 @@ class FullyConnectedLayer(torch.nn.Module):
         if self.activation == 'linear' and b is not None:
             x = torch.addmm(b.unsqueeze(0), x, w.t())
         else:
+            print(x.shape, w.shape)
             x = x.matmul(w.t())
             x = bias_act.bias_act(x, b, act=self.activation)
         return x
