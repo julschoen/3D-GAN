87ae2e94625c6e67887790cef357e5210a553b32
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-14 14:34:44 +0100

    Update stylegan.py

--- stylegan.py
+++ stylegan.py
@@ -173,6 +173,54 @@ class Conv3DMod(nn.Module):
         x = x.reshape(-1, self.filters, h, w, d)
         return x
 
+class Conv3dLayer(torch.nn.Module):
+    def __init__(self,
+        in_channels,                    # Number of input channels.
+        out_channels,                   # Number of output channels.
+        kernel_size,                    # Width and height of the convolution kernel.
+        bias            = True,         # Apply additive bias before the activation function?
+        activation      = 'linear',     # Activation function: 'relu', 'lrelu', etc.
+        up              = 1,            # Integer upsampling factor.
+        down            = 1,            # Integer downsampling factor.
+        resample_filter = [1,3,3,1],    # Low-pass filter to apply when resampling activations.
+        conv_clamp      = None,         # Clamp the output to +-X, None = disable clamping.
+        channels_last   = False,        # Expect the input to have memory_format=channels_last?
+        trainable       = True,         # Update the weights of this layer during training?
+    ):
+        super().__init__()
+        self.activation = activation
+        self.up = up
+        self.down = down
+        self.conv_clamp = conv_clamp
+        self.register_buffer('resample_filter', upfirdn2d.setup_filter(resample_filter))
+        self.padding = kernel_size // 2
+        self.weight_gain = 1 / np.sqrt(in_channels * (kernel_size ** 2))
+        self.act_gain = bias_act.activation_funcs[activation].def_gain
+
+        memory_format = torch.channels_last if channels_last else torch.contiguous_format
+        weight = torch.randn([out_channels, in_channels, kernel_size, kernel_size, kernel_size]).to(memory_format=memory_format)
+        bias = torch.zeros([out_channels]) if bias else None
+        if trainable:
+            self.weight = torch.nn.Parameter(weight)
+            self.bias = torch.nn.Parameter(bias) if bias is not None else None
+        else:
+            self.register_buffer('weight', weight)
+            if bias is not None:
+                self.register_buffer('bias', bias)
+            else:
+                self.bias = None
+
+    def forward(self, x, gain=1):
+        w = self.weight * self.weight_gain
+        b = self.bias.to(x.dtype) if self.bias is not None else None
+        flip_weight = (self.up == 1) # slightly faster
+        x = conv2d_resample.conv2d_resample(x=x, w=w.to(x.dtype), f=self.resample_filter, up=self.up, down=self.down, padding=self.padding, flip_weight=flip_weight)
+
+        act_gain = self.act_gain * gain
+        act_clamp = self.conv_clamp * gain if self.conv_clamp is not None else None
+        x = bias_act.bias_act(x, b, act=self.activation, gain=act_gain, clamp=act_clamp)
+        return x
+
 class GeneratorBlock(nn.Module):
     def __init__(self, latent_dim, in_channels, out_channels, resolution, upsample = True):
         super().__init__()
@@ -328,6 +376,8 @@ class Discriminator(nn.Module):
         chan_last = filters[-1]
         latent_dim = 4**3 * chan_last
 
+
+        self.act = nn.LeakyReLU(0.2, inplace=True)
         self.final_conv = nn.Conv3d(chan_last, chan_last, 1, padding=1)
         self.flatten = Flatten()
         self.to_logit = nn.Linear(latent_dim, 1)
@@ -338,7 +388,7 @@ class Discriminator(nn.Module):
         for block in self.blocks:
             x = block(x)
 
-        x = self.final_conv(x)
+        x = self.act(self.final_conv(x))
         x = self.flatten(x)
         x = self.to_logit(x)
         return x.squeeze()
