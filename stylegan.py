5cb75b048628a3362e2fcd1dc8d08e83b1c007b8
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-15 14:09:30 +0100

    Revert "Update stylegan.py"
    This reverts commit 3ffb02fc5a2ab4a911e71ed1c63c3cf8f3607419.

--- stylegan.py
+++ stylegan.py
@@ -113,9 +113,9 @@ class MappingNetwork(torch.nn.Module):
                 self.w_avg.copy_(x.detach().mean(dim=0).lerp(self.w_avg, self.w_avg_beta))
 
         # Broadcast.
-        #if self.num_ws is not None:
-        #    with torch.autograd.profiler.record_function('broadcast'):
-        #        x = x.unsqueeze(1).repeat([1, self.num_ws, 1])
+        if self.num_ws is not None:
+            with torch.autograd.profiler.record_function('broadcast'):
+                x = x.unsqueeze(1).repeat([1, self.num_ws, 1])
 
         return x
 
@@ -285,9 +285,11 @@ class SynthesisNetwork(nn.Module):
         out = OutBlock(self.latent_dim, out_channels)
         self.blocks.append(out)
 
-    def forward(self, style):
+    def forward(self, styles):
         x = self.initial_block.expand(styles.shape[0], -1, -1, -1, -1)
-        for block in self.blocks:
+        styles = styles.transpose(0, 1)
+
+        for style, block in zip(styles, self.blocks):
             x = block(x, style)
 
         return torch.tanh(x)
