ad60501c6c3f92b19a0d356d343e04d019645373
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-15 13:10:19 +0100

    Update stylegan.py

--- stylegan.py
+++ stylegan.py
@@ -277,7 +277,7 @@ class SynthesisNetwork(nn.Module):
         filters = [network_capacity * (2 ** (i + 1)) for i in range(self.num_layers)][::-1]
         filters[-1] = 1
         channels_dict = {res: min(filters[i], fmap_max) for i, res in enumerate(self.block_resolutions)}
-
+        print(channels_dict)
         init_res = self.block_resolutions[0]
         init_channels = channels_dict[init_res]
         self.initial_block = nn.Parameter(torch.randn((1, init_channels, init_res, init_res, init_res)))
