e7c56487572d520ea68657a7ec49855e129677ba
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-15 12:55:56 +0100

    Update stylegan.py

--- stylegan.py
+++ stylegan.py
@@ -274,6 +274,7 @@ class SynthesisNetwork(nn.Module):
         self.latent_dim = w_dim
         self.num_layers = int(log2(self.image_size)-1)
         self.block_resolutions = [2 ** (i+2) for i in range(self.num_layers)]
+        print(block_resolutions)
         filters = [network_capacity * (2 ** (i + 1)) for i in range(self.num_layers)][::-1]
         filters[-1] = 1
         channels_dict = {res: min(filters[i], fmap_max) for i, res in enumerate(self.block_resolutions)}
