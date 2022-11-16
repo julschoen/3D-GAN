6354f6466cb863e00410b4843ac39b1222e4f8cb
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-16 16:47:35 +0100

    Update style_test.py

--- style_test.py
+++ style_test.py
@@ -275,7 +275,7 @@ def modulated_conv3d(
     dcoefs = None
     if demodulate or fused_modconv:
         w = weight.unsqueeze(0) # [NOIkk]
-        w = w * styles.reshape(batch_size, 1, -1, 1, 1, 1) # [NOIkk]
+        w = w * styles.reshape(batch_size, 1, -1) # [NOIkk]
     if demodulate:
         dcoefs = (w.square().sum(dim=[2,3,4]) + 1e-8).rsqrt() # [NO]
     if demodulate and fused_modconv:
