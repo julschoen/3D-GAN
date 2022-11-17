bb23af31bcebf2efcca3babbeaece9d49ef1cd76
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-17 11:20:08 +0100

    Update style_test.py

--- style_test.py
+++ style_test.py
@@ -146,8 +146,7 @@ def _upfirdn3d_ref(x, f, up=1, down=1, padding=0, flip_filter=False, gain=1):
     # Pad or crop.
     x = torch.nn.functional.pad(x, [max(padx0, 0), max(padx1, 0), max(pady0, 0), max(pady1, 0), max(padz0, 0), max(padz1, 0)])
     x = x[:, :, max(-pady0, 0) : x.shape[2] - max(-pady1, 0), max(-padx0, 0) : x.shape[3] - max(-padx1, 0), max(-padz0, 0) : x.shape[4] - max(-padz1, 0)]
-
-    
+    print(x.shape)
 
     # Setup filter.
     f = f * (gain ** (f.ndim / 2))
@@ -162,6 +161,7 @@ def _upfirdn3d_ref(x, f, up=1, down=1, padding=0, flip_filter=False, gain=1):
 
     # Downsample by throwing away pixels.
     x = x[:, :, ::downy, ::downx, ::downz]
+    print(x.shape)
     return x
 
 def conv3d_resample(x, w, f=None, up=1, down=1, padding=0, groups=1, flip_weight=True, flip_filter=False):
