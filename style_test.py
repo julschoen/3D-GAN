491341d336f45a2d063c0074ced5a99a0b23de54
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-17 17:24:32 +0100

    Update style_test.py

--- style_test.py
+++ style_test.py
@@ -147,7 +147,7 @@ def _upfirdn3d_ref(x, f, up=1, down=1, padding=0, flip_filter=False, gain=1):
     # Convolve with the filter.
     print(f.shape)
     print(x.shape)
-    f = f[np.newaxis,np.newaxis,np.newaxis].repeat([num_channels,num_channels,1] + [1] * f.ndim)
+    #f = f[np.newaxis,np.newaxis,np.newaxis].repeat([num_channels,num_channels,1] + [1] * f.ndim)
     f = f.to(dtype=x.dtype, device=x.device)
     x = F.conv3d(x, f)
     # Downsample by throwing away pixels.
