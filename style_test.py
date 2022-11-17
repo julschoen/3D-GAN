175c19fff81205d1081f239157785b5e7ec9e5cf
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-17 14:58:57 +0100

    Update style_test.py

--- style_test.py
+++ style_test.py
@@ -382,8 +382,8 @@ def modulated_conv3d(
         w = w * dcoefs.reshape(batch_size, -1, 1, 1, 1) # [NOIkk]
     # Execute by scaling the activations before and after the convolution.
     if not fused_modconv:
-        x = x * styles.to(x.dtype).reshape(batch_size, -1, 1, 1, 1)
         print(x)
+        x = x * styles.to(x.dtype).reshape(batch_size, -1, 1, 1, 1)
         x = conv3d_resample(x=x, w=weight.to(x.dtype), f=resample_filter, up=up, down=down, padding=padding, flip_weight=flip_weight)
         if demodulate and noise is not None:
             x = fma(x, dcoefs.to(x.dtype).reshape(batch_size, -1, 1, 1, 1), noise.to(x.dtype))
