3655a5572b5685f79cf1464497fa1bbbe8b9c1a1
julschoen <31760205+julschoen@users.noreply.github.com>
2022-11-30 15:48:56 +0100

    Update stylegan.py

--- stylegan.py
+++ stylegan.py
@@ -3,6 +3,102 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class StyleGAN2Loss(Loss):
+    def __init__(self, device, G_mapping, G_synthesis, D, augment_pipe=None, style_mixing_prob=0.9, r1_gamma=10, pl_batch_shrink=2, pl_decay=0.01, pl_weight=2):
+        super().__init__()
+        self.device = device
+        self.G_mapping = G_mapping
+        self.G_synthesis = G_synthesis
+        self.D = D
+        self.augment_pipe = augment_pipe
+        self.style_mixing_prob = style_mixing_prob
+        self.r1_gamma = r1_gamma
+        self.pl_batch_shrink = pl_batch_shrink
+        self.pl_decay = pl_decay
+        self.pl_weight = pl_weight
+        self.pl_mean = torch.zeros([], device=device)
+
+    def run_G(self, z):
+        ws = self.G_mapping(z)
+        if self.style_mixing_prob > 0:
+            with torch.autograd.profiler.record_function('style_mixing'):
+                cutoff = torch.empty([], dtype=torch.int64, device=ws.device).random_(1, ws.shape[1])
+                cutoff = torch.where(torch.rand([], device=ws.device) < self.style_mixing_prob, cutoff, torch.full_like(cutoff, ws.shape[1]))
+                ws[:, cutoff:] = self.G_mapping(torch.randn_like(z), skip_w_avg_update=True)[:, cutoff:]
+        img = self.G_synthesis(ws)
+        return img, ws
+
+    def run_D(self, img):
+        logits = self.D(img)
+        return logits
+
+    def step_D(self, step, real_img, gen_z, gain=1):
+        loss_Dgen = 0
+        do_Dr1   = (step % 16) == 0 and (self.r1_gamma != 0)
+        
+        with torch.autograd.profiler.record_function('Dgen_forward'):
+            gen_img, _gen_ws = self.run_G(gen_z)
+            gen_logits = self.run_D(gen_img)
+            loss_Dgen = torch.nn.functional.softplus(gen_logits)
+        with torch.autograd.profiler.record_function('Dgen_backward'):
+            loss_Dgen.mean().mul(gain).backward()
+
+        # Dmain: Maximize logits for real images.
+        # Dr1: Apply R1 regularization.
+        name = 'Dreal_Dr1' if do_Dr1 else 'Dreal'
+        with torch.autograd.profiler.record_function(name + '_forward'):
+            real_img_tmp = real_img.detach().requires_grad_(do_Dr1)
+            real_logits = self.run_D(real_img_tmp)
+
+            loss_Dreal = 0
+            loss_Dreal = torch.nn.functional.softplus(-real_logits) # -log(sigmoid(real_logits))
+
+            loss_Dr1 = 0
+            if do_Dr1:
+                with torch.autograd.profiler.record_function('r1_grads'):
+                    r1_grads = torch.autograd.grad(outputs=[real_logits.sum()], inputs=[real_img_tmp], create_graph=True, only_inputs=True)[0]
+                r1_penalty = r1_grads.square().sum([1,2,3])
+                loss_Dr1 = r1_penalty * (self.r1_gamma / 2)
+
+        with torch.autograd.profiler.record_function(name + '_backward'):
+            (real_logits * 0 + loss_Dreal + loss_Dr1).mean().mul(gain).backward()
+
+        return (real_logits * 0 + loss_Dreal + loss_Dr1).mean().mul(gain).item(), loss_Dgen.mean().mul(gain).item()
+
+
+    def stepG(self, step, gen_z, gain=1):
+        do_Gpl   = (step % 16) == 0
+
+        with torch.autograd.profiler.record_function('Gmain_forward'):
+            gen_img, _gen_ws = self.run_G(gen_z) # May get synced by Gpl.
+            gen_logits = self.run_D(gen_img)
+            loss_Gmain = torch.nn.functional.softplus(-gen_logits)
+        with torch.autograd.profiler.record_function('Gmain_backward'):
+            loss_Gmain.mean().mul(gain).backward()
+
+        # Gpl: Apply path length regularization.
+        if do_Gpl:
+            with torch.autograd.profiler.record_function('Gpl_forward'):
+                batch_size = gen_z.shape[0] // self.pl_batch_shrink
+                gen_img, gen_ws = self.run_G(gen_z[:batch_size])
+                pl_noise = torch.randn_like(gen_img) / np.sqrt(gen_img.shape[2] * gen_img.shape[3])
+                with torch.autograd.profiler.record_function('pl_grads'):
+                    pl_grads = torch.autograd.grad(outputs=[(gen_img * pl_noise).sum()], inputs=[gen_ws], create_graph=True, only_inputs=True)[0]
+                pl_lengths = pl_grads.square().sum(2).mean(1).sqrt()
+                pl_mean = self.pl_mean.lerp(pl_lengths.mean(), self.pl_decay)
+                self.pl_mean.copy_(pl_mean.detach())
+                pl_penalty = (pl_lengths - pl_mean).square()
+                loss_Gpl = pl_penalty * self.pl_weight
+            with torch.autograd.profiler.record_function('Gpl_backward'):
+                (gen_img[:, 0, 0, 0, 0] * 0 + loss_Gpl).mean().mul(gain).backward()
+
+        loss = (gen_img[:, 0, 0, 0, 0] * 0 + loss_Gpl).mean().mul(gain) + loss_Gmain.mean().mul(gain) if do_Gpl else loss_Gmain.mean().mul(gain)
+
+        return loss.item()
+
+        
+        
+
 activation_funcs = {
     'linear':   lambda x: x,
     'relu':     lambda x: torch.nn.functional.relu(x),
