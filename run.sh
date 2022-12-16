4ede7194f89a9276e290ce3e8d04d5917e8a5316
julschoen <julianschoen@gmx.de>
2022-12-16 20:06:56 +0100

    run_script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-../Temp-GAN/gan/bin/python main.py --log_dir=Style --iterD=2 --lrG=1e-4 --stylegan=True --hinge=True
-
+../Temp-GAN/gan/bin/python main.py --log_dir=Style1Hinge --hinge=True --filterD=2048 --lrG=2e-4 --lrD=2e-4 --stylegan=True --iterD=1
+../Temp-GAN/gan/bin/python main.py --log_dir=Style1HingeTTUR --hinge=True --filterD=2048 --lrG=2e-4 --lrD=1e-4 --stylegan=True --iterD=2
