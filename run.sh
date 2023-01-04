8fc5cb1765d818ce77ce35eadb1b98410cdb13dc
julschoen <julianschoen@gmx.de>
2023-01-04 11:31:15 +0100

    run

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-../Temp-GAN/gan/bin/python main.py --log_dir=Style1Hinge --hinge=True --filterD=2048 --lrG=2e-4 --lrD=2e-4 --stylegan=True --iterD=1
-../Temp-GAN/gan/bin/python main.py --log_dir=Style1HingeTTUR --hinge=True --filterD=2048 --lrG=2e-4 --lrD=1e-4 --stylegan=True --iterD=2
+../Temp-GAN/gan/bin/python main.py --log_dir=Style --hinge=True --filterD=2048 --lrG=1e-4 --lrD=1e-4 --stylegan=True --iterD=1
