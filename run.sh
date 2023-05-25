790c9e9b20047a064548c4f20e1b5f1565994097
julschoen <julianschoen@gmx.de>
2023-05-25 10:00:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/WGAN --dcgan=True --data_path=train_brats.npz --lrG=1e-4 --iterD=5
-../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/WGAN1 --dcgan=True --data_path=train_brats.npz --lrG=1e-4 --iterD=5
-../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/WGAN2 --dcgan=True --data_path=train_brats.npz --lrG=1e-4 --iterD=5
+../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/SNGAN --dcgan=True --data_path=train_brats.npz --lrG=1e-4 --iterD=5 --sngan=True
+../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/SNGAN1 --dcgan=True --data_path=train_brats.npz --lrG=1e-4 --iterD=5 --sngan=True
+../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/SNGAN2 --dcgan=True --data_path=train_brats.npz --lrG=1e-4 --iterD=5 --sngan=True
