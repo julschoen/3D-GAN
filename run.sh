a48ba4bc11430b724b9467c954d0ad59daa6bfcc
julschoen <julianschoen@gmx.de>
2023-05-29 13:04:17 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/ResSN  --sngan=True --data_path=brats_train --filterD=64 --filterG=64
-../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/ResSN1 --sngan=True --data_path=brats_train --filterD=64 --filterG=64
-../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/ResSN2 --sngan=True --data_path=brats_train --filterD=64 --filterG=64
+../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/ResSN  --sngan=True --data_path=brats_train --filterD=64 --filterG=64 --hinge=True
+../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/ResSN1 --sngan=True --data_path=brats_train --filterD=64 --filterG=64 --hinge=True
+../Temp-GAN/gan/bin/python main.py --log_dir=BRATS_GANs/ResSN2 --sngan=True --data_path=brats_train --filterD=64 --filterG=64 --hinge=True
