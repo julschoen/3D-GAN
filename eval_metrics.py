3aa0b18d1246bc845d30691d1a9b8ef90a5169af
julschoen <julianschoen@gmx.de>
2023-08-28 10:34:17 +0200

    Update eval_metrics.py

--- eval_metrics.py
+++ eval_metrics.py
@@ -15,41 +15,128 @@ def eval(params):
 	if params.ngpu > 1:
 		fid_model = nn.DataParallel(fid_model)
 
-	for i, data in enumerate(generator):
-		if i == 0:
-			x1 = data.unsqueeze(dim=1)
-		elif i == 1:
-			x2 = data.unsqueeze(dim=1)
-		else:
-			break
+
+	ssims = []
+	mmds = []
+	fas = []
+	fcs = []
+	fss = []
+	f3 = []
+
+	for _ in range(3):
+		for i, data in enumerate(generator):
+			if i == 0:
+				x1 = data.unsqueeze(dim=1)
+			elif i == 1:
+				x2 = data.unsqueeze(dim=1)
+			else:
+				break
 
 
-	s,f = ssim(x1.cpu(),x2.cpu()), fid_3d(fid_model, x1, x2)
-	m = mmd(x1.cpu(), x2.cpu())
-	fa, fc, fs = fid(x1, x2, params.device)
+		s,f = ssim(x1.cpu(),x2.cpu()), fid_3d(fid_model, x1, x2)
+		m = mmd(x1.cpu(), x2.cpu())
+		fa, fc, fs = fid(x1, x2, params.device)
+
+		ssims.append(s)
+		mmds.append(m)
+		fas.append(fa)
+		fcs.append(fc)
+		fss.append(fs)
+		f3.append(f)
+
+	ssims = np.array(ssims)
+	mmds = np.array(mmds)
+	fas = np.array(fas)
+	fcs = np.array(fcs)
+	fss = np.array(fss)
+	f3 = np.array(f3)
 		
 	print('Metrics vs 2 Data Batches')
-	print(f'SSIM: {s:.2f} MMD: {m:.2f} FID ax: {fa:.1f} cor: {fc:.1f} sag: {fs:.1f} 3D: {f:.2f}')
+	print(f'SSIM: {ssims.mean():.2f}+-{ssims.std():.2f}'+ 
+			f'\tMMD: {mmds.mean():.2f}+-{mmds.std():.2f}'+
+			f'FID ax: {fas.mean():.1f}+-{fas.std():.1f}'+
+			f'\tFID cor: {fcs.mean():.1f}+-{fcs.std():.1f}'+
+			f'\tFID sag: {fss.mean():.1f}+-{fss.std():.1f}'#+
+			f'\t3d-FID: {f3.mean():.2f}+-{f3.std():.2f}', flush=True
+			)
 
-	x1 = torch.randn(32, 1,128, 128, 128, dtype=torch.float, device=params.device)
-	x2 = torch.randn(32, 1,128, 128, 128, dtype=torch.float, device=params.device)
 
-	s,f = ssim(x1.cpu(),x2.cpu()), fid_3d(fid_model, x1, x2)
-	m = mmd(x1.cpu(), x2.cpu())
-	fa, fc, fs = fid(x1, x2, params.device)
+	ssims = []
+	mmds = []
+	fas = []
+	fcs = []
+	fss = []
+	f3 = []
+
+	for _ in range(3):
+		x1 = torch.randn(32, 1,128, 128, 128, dtype=torch.float, device=params.device)
+		x2 = torch.randn(32, 1,128, 128, 128, dtype=torch.float, device=params.device)
+
+		s,f = ssim(x1.cpu(),x2.cpu()), fid_3d(fid_model, x1, x2)
+		m = mmd(x1.cpu(), x2.cpu())
+		fa, fc, fs = fid(x1, x2, params.device)
+
+		ssims.append(s)
+		mmds.append(m)
+		fas.append(fa)
+		fcs.append(fc)
+		fss.append(fs)
+		f3.append(f)
+
+	ssims = np.array(ssims)
+	mmds = np.array(mmds)
+	fas = np.array(fas)
+	fcs = np.array(fcs)
+	fss = np.array(fss)
+	f3 = np.array(f3)
 		
 	print('Metrics vs 2 RandN')
-	print(f'SSIM: {s:.2f} MMD: {m:.2f} FID ax: {fa:.1f} cor: {fc:.1f} sag: {fs:.1f} 3D: {f:.2f}')
+	print(f'SSIM: {ssims.mean():.2f}+-{ssims.std():.2f}'+ 
+			f'\tMMD: {mmds.mean():.2f}+-{mmds.std():.2f}'+
+			f'FID ax: {fas.mean():.1f}+-{fas.std():.1f}'+
+			f'\tFID cor: {fcs.mean():.1f}+-{fcs.std():.1f}'+
+			f'\tFID sag: {fss.mean():.1f}+-{fss.std():.1f}'#+
+			f'\t3d-FID: {f3.mean():.2f}+-{f3.std():.2f}', flush=True
+			)
 
-	x1 = torch.randn(32, 1,128, 128, 128, dtype=torch.float, device=params.device)
-	x2 = torch.rand(32, 1,128, 128, 128, dtype=torch.float, device=params.device)
 
-	s,f = ssim(x1.cpu(),x2.cpu()), fid_3d(fid_model, x1, x2)
-	m = mmd(x1.cpu(), x2.cpu())
-	fa, fc, fs = fid(x1, x2, params.device)
+	ssims = []
+	mmds = []
+	fas = []
+	fcs = []
+	fss = []
+	f3 = []
+
+	for _ in range(3)
+		x1 = torch.randn(32, 1,128, 128, 128, dtype=torch.float, device=params.device)
+		x2 = torch.rand(32, 1,128, 128, 128, dtype=torch.float, device=params.device)
+
+		s,f = ssim(x1.cpu(),x2.cpu()), fid_3d(fid_model, x1, x2)
+		m = mmd(x1.cpu(), x2.cpu())
+		fa, fc, fs = fid(x1, x2, params.device)
+
+		ssims.append(s)
+		mmds.append(m)
+		fas.append(fa)
+		fcs.append(fc)
+		fss.append(fs)
+		f3.append(f)
+
+	ssims = np.array(ssims)
+	mmds = np.array(mmds)
+	fas = np.array(fas)
+	fcs = np.array(fcs)
+	fss = np.array(fss)
+	f3 = np.array(f3)
 		
 	print('Metrics vs RandN/RandU')
-	print(f'SSIM: {s:.2f} MMD: {m:.2f} FID ax: {fa:.1f} cor: {fc:.1f} sag: {fs:.1f} 3D: {f:.2f}')
+	print(f'SSIM: {ssims.mean():.2f}+-{ssims.std():.2f}'+ 
+			f'\tMMD: {mmds.mean():.2f}+-{mmds.std():.2f}'+
+			f'FID ax: {fas.mean():.1f}+-{fas.std():.1f}'+
+			f'\tFID cor: {fcs.mean():.1f}+-{fcs.std():.1f}'+
+			f'\tFID sag: {fss.mean():.1f}+-{fss.std():.1f}'#+
+			f'\t3d-FID: {f3.mean():.2f}+-{f3.std():.2f}', flush=True
+			)
 
 def main():
 	parser = argparse.ArgumentParser()
